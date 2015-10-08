c59b7fdc5c9f33affa7978a100b0a75f30e5b5eb
Steve <stevenlyall@gmail.com>
2015-10-08 11:13:54 -0700

    handle arguments, parse into int[]

--- src/multithreadstats/MultiThreadStats.java
+++ src/multithreadstats/MultiThreadStats.java
@@ -2,7 +2,26 @@ package multithreadstats;
 
 public class MultiThreadStats {
 
+    static int[] inputNumbers;
+
     public static void main(String[] args) {
-	// write your code here
+        if (args.length < 1) {
+            System.out.println("Must provide at least one integer argument.\nUsage: MultiThreadStats <number> <number> ...");
+        }
+
+        inputNumbers = new int[args.length];
+
+        try {
+            for (int i = 0; i < args.length; i++) {
+                inputNumbers[i] = Integer.parseInt(args[i]);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+
+        // TODO
+
     }
 }
